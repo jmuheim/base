7612f040aa524929ea8f329f10b3bdad967a404c
Joshua Muheim <joshua.muheim@sientia.ch>
2013-12-01 20:43:33 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Transition::Application.config.secret_key_base = 'f4477ea10b1be4371a20f9b4be85f84dcc45d22d5e8cc35e282412826ba7bc4667df27d7633108b684fede98e70188d255fec0d9020dbc344d8cbbd2ed38f350'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Transition::Application.config.session_store :cookie_store, key: '_transition_session'
