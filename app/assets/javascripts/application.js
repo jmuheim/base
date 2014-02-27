d12ea2de765f4c1a819d61bb43170915f5cf94e2
Joshua Muheim <joshua.muheim@sientia.ch>
2014-02-27 22:43:43 +0100

    Move bootstrap import below jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap
 //= require_tree .
