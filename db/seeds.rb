2e34980f351defac176539e22d18087483a50e86
Joshua Muheim <joshua.muheim@sientia.ch>
2014-02-28 11:26:34 +0100

    Change email for user josh in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,1 @@
-User.create! username: 'josh', email: 'forum@josh.ch', password: 'joshjosh', confirmed_at: Time.now
+User.create! username: 'josh', email: 'josh@josh.ch', password: 'joshjosh', confirmed_at: Time.now
