198b7abf774c4a21a5a7ef8b56751866bcc5fbe6
Joshua Muheim <joshua.muheim@sientia.ch>
2014-02-28 13:01:51 +0100

    Initialize rolify

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,7 @@
 #
 
 class User < ActiveRecord::Base
+  rolify
   # Include default devise modules. Others available are:
   # :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
