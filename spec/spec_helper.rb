edadc64fa7585cbf54e7262873ad54f27cf69d60
Joshua Muheim <joshua.muheim@sientia.ch>
2014-03-01 12:36:18 +0100

    Require cancan matchers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,7 @@ require 'capybara/poltergeist'
 require 'turnip/capybara'
 require 'capybara-screenshot/rspec'
 require 'email_spec'
+require 'cancan/matchers'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
