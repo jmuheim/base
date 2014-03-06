8c3981aa2e3f6b2bf73855ff382e35c22e131ff0
Joshua Muheim <joshua.muheim@sientia.ch>
2014-03-06 20:42:32 +0100

    Add ErrorsController for gaffe gem (not used at the moment)

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,11 +1,10 @@
 class ErrorsController < ApplicationController
+  include Gaffe::Errors
+
   def show
-    render status_code.to_s, status: status_code
-  end
-
-  protected
-
-  def status_code
-    params[:code] || 500
+    # The following variables are available:
+    @exception # The encountered exception (Eg. `#<ActiveRecord::NotFound …>`)
+    @status_code # The status code we should return (Eg. `404`)
+    @rescue_response # The "standard" name for the status code (Eg. `:not_found`)
   end
 end
