fbe337993610c11423cefc95c318b2ab9e4caf69
Joshua Muheim <joshua.muheim@sientia.ch>
2014-03-11 11:39:20 +0100

    Disable using own errors controller as it doesn't seem to work yet

--- config/initializers/gaffe.rb
+++ config/initializers/gaffe.rb
@@ -1,6 +1,8 @@
 # Easy error rescuing, see https://github.com/mirego/gaffe
 
-Gaffe.configure do |config|
-  config.errors_controller = ErrorsController
-end
+# Doesn't seem to work yet, see https://github.com/mirego/gaffe/issues/11
+# Gaffe.configure do |config|
+#   config.errors_controller = ErrorsController
+# end
+
 Gaffe.enable!
