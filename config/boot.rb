8b8d2ccc100d983901ce4d42aa063ed5b8da5237
Joshua Muheim <joshua.muheim@sientia.ch>
2014-03-19 09:19:49 +0100

    Set webrick port in a more current way

--- config/boot.rb
+++ config/boot.rb
@@ -3,13 +3,11 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
-# Change default port of development server, see http://stackoverflow.com/questions/10676680
+# Change default port of development server, see http://stackoverflow.com/questions/18103316
 require 'rails/commands/server'
-module Rails
-  class Server
-    alias :default_options_alias :default_options
-    def default_options
-      default_options_alias.merge!(Port: 3001)
-    end    
+module DefaultOptions
+  def default_options
+    super.merge!(Port: 3001)
   end
 end
+Rails::Server.send(:prepend, DefaultOptions)
