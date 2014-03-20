73cbaddfe23ba54bfd9f31e1ee2671fa231f5afd
Joshua Muheim <joshua.muheim@sientia.ch>
2014-03-20 12:08:11 +0100

    We need a uniform way to generate icon tags

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe ApplicationHelper do
+  describe '#icon(name)' do
+    subject { icon 'not-ok' }
+
+    it { should have_css 'span.glyphicon.glyphicon-not-ok' }
+    it { should have_css 'span span.hide-text' }
+    it { should have_content 'Not Ok' }
+  end
+
+  describe '#icon(name, content)' do
+    subject { icon 'not-ok', 'This is the content' }
+    it { should have_content 'This is the content' }
+  end
+end
