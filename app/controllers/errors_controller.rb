65fb16086b5a3da0f04610905e7b46fd739d3db7
Joshua Muheim <joshua.muheim@sientia.ch>
2014-03-20 13:13:57 +0100

    Remove gaffe errors controller for the time being.
    TODO: We need an error layout!

--- app/controllers/errors_controller.rb
+++
@@ -1,8 +0,0 @@
-class ErrorsController < ApplicationController
-  include Gaffe::Errors
-  layout 'application'
-
-  def show
-    render "errors/#{@rescue_response}", status: @status_code
-  end
-end
--- config/initializers/gaffe.rb
+++ config/initializers/gaffe.rb
@@ -1,8 +1,2 @@
 # Easy error rescuing, see https://github.com/mirego/gaffe
-
-# Doesn't seem to work yet, see https://github.com/mirego/gaffe/issues/11
-# Gaffe.configure do |config|
-#   config.errors_controller = ErrorsController
-# end
-
 Gaffe.enable!
