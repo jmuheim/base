e53827f46e38d1c30ae90d254cdb4aac88a297e1
Joshua Muheim <joshua.muheim@sientia.ch>
2014-03-26 15:24:12 +0100

    Re-add address to fix OpenSSL::SSL::SSLError

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,7 @@ end
 
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
+  address:              'sirius.uberspace.de',
   port:                 587,
   domain:               'sirius.uberspace.de',
   user_name:            'base-mailer@sirius.uberspace.de',
