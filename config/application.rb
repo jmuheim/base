4aa0bdb1975c96089fe57bee30c0db6ce47d95b9
Joshua Muheim <joshua.muheim@sientia.ch>
2014-04-08 21:55:12 +0200

    Set default locale to "de"

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Base
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :de
+    config.i18n.default_locale = :en
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 
     config.sass.preferred_syntax = :sass
