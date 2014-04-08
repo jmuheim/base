795f89579bae4cd781b3b9379eba35dd06a2b47d
Joshua Muheim <joshua.muheim@sientia.ch>
2014-04-08 22:15:10 +0200

    Add i18n-tasks specs

---
+++ spec/i18n_keys_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+require 'i18n/tasks'
+
+describe 'Translation keys'  do
+  let(:i18n) { I18n::Tasks::BaseTask.new }
+
+  it 'are all present' do
+    expect(i18n.missing_keys).to have(0).keys
+  end
+
+  it 'are all used' do
+    expect(i18n.unused_keys).to have(0).keys
+  end
+end
