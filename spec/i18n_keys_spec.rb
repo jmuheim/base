50f19be692bdd3b6c258d624a13b3a4643ac187b
Joshua Muheim <joshua.muheim@sientia.ch>
2014-04-11 11:30:55 +0200

    Refactor i18n specs

--- spec/i18n_keys_spec.rb
+++ spec/i18n_keys_spec.rb
@@ -1,14 +1,16 @@
 require 'spec_helper'
 require 'i18n/tasks'
 
-describe 'Translation keys'  do
-  let(:i18n) { I18n::Tasks::BaseTask.new }
+describe 'I18n' do
+  before { @i18n = I18n::Tasks::BaseTask.new }
 
-  it 'are all present' do
-    expect(i18n.missing_keys).to have(0).keys
+  it "doesn't have any missing keys" do
+    count = @i18n.missing_keys.count
+    fail "There are #{count} missing i18n keys! Run 'i18n-tasks missing' for more details." if count > 0
   end
 
-  it 'are all used' do
-    expect(i18n.unused_keys).to have(0).keys
+  it "doesn't have any unused keys" do
+    count = @i18n.unused_keys.count
+    fail "There are #{count} unused i18n keys! Run 'i18n-tasks unused' for more details." if count > 0
   end
 end
