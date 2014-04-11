29e79a4e60b39183e96f0dedb17391c354867f2a
Joshua Muheim <joshua.muheim@sientia.ch>
2014-04-11 12:22:39 +0200

    Refactor users/list feature

--- spec/support/sign_in.rb
+++ spec/support/sign_in.rb
@@ -1,9 +1,25 @@
-module AcceptanceTestsSignInHelper
-  include Warden::Test::Helpers
-  Warden.test_mode!
+module ControllerSpecSignInHelper
+  def sign_in_as(user)
+    sign_in(user)
+    user
+  end
 end
 
 RSpec.configure do |config|
-  config.include Devise::TestHelpers, type: :controller
-  config.include AcceptanceTestsSignInHelper, type: :feature
+  config.include ControllerSpecSignInHelper, type: :controller
+end
+
+module FeatureSpecSignInHelper
+  # See https://github.com/plataformatec/devise/wiki/How-To%3a-Test-with-Capybara
+  include Warden::Test::Helpers
+  Warden.test_mode!
+
+  def sign_in_as(user)
+    login_as(user)
+    user
+  end
+end
+
+RSpec.configure do |config|
+  config.include FeatureSpecSignInHelper, type: :feature
 end
