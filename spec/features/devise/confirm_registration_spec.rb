9aa96b7a36215eafa6bce0f75847b305e7a02663
Joshua Muheim <joshua.muheim@sientia.ch>
2014-04-13 23:53:24 +0200

    Refactor confirm registration feature spec

---
+++ spec/features/devise/confirm_registration_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe 'Confirming registration' do
+  before { @user = create :user, :donald, confirmed_at: nil }
+
+  it 'confirms the registration' do
+    visit_in_email('Confirm my account', 'donald@example.com')
+
+    expect(page).to have_content 'Your account was successfully confirmed.'
+  end
+end
--- spec/features/devise/sign_up_spec.rb
+++ spec/features/devise/sign_up_spec.rb
@@ -1,19 +1,17 @@
 require 'spec_helper'
 
 describe 'Signing up' do
-  context 'with valid data' do
-    it 'signs up a new user' do
-      visit new_user_registration_path
+  it 'signs up a new user' do
+    visit new_user_registration_path
 
-      fill_in 'user_name',                  with: 'newuser'
-      fill_in 'user_email',                 with: 'newuser@example.com'
-      fill_in 'user_password',              with: 'somegreatpassword'
-      fill_in 'user_password_confirmation', with: 'somegreatpassword'
-      click_button 'Sign up'
+    fill_in 'user_name',                  with: 'newuser'
+    fill_in 'user_email',                 with: 'newuser@example.com'
+    fill_in 'user_password',              with: 'somegreatpassword'
+    fill_in 'user_password_confirmation', with: 'somegreatpassword'
+    click_button 'Sign up'
 
-      expect(page).to have_content 'You have signed up successfully.'
-      expect(unread_emails_for('newuser@example.com').size).to eq 1
-      expect(page).to have_link 'Log out'
-    end
+    expect(page).to have_content 'You have signed up successfully.'
+    expect(unread_emails_for('newuser@example.com').size).to eq 1
+    expect(page).to have_link 'Log out'
   end
 end
