0628de28a59ab0d13114b93262ac172c90cf3dab
Joshua Muheim <joshua.muheim@sientia.ch>
2014-04-13 21:34:35 +0200

    Refactor delete user spec

---
+++ spec/features/users/delete_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe 'Deleting user' do
+  before { @user = create :user, :donald }
+
+  context 'signed in as user' do
+    before { login_as(@user) }
+
+    it 'does not grant permission to delete own user' do
+      visit_delete_path_for(@user)
+
+      expect(page).to have_content '403 - Forbidden'
+      expect(page.driver.status_code).to eq 403
+    end
+  end
+
+  context 'signed in as admin' do
+    before do
+      admin = create :admin, :scrooge
+      login_as(admin)
+    end
+
+    it 'grants permission to delete other user' do
+      visit_delete_path_for(@user)
+
+      expect(page.driver.status_code).to eq 200
+    end
+  end
+end
