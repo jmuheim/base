e695bc3e290935d08f938110c661370bbcac6c7b
Joshua Muheim <joshua.muheim@sientia.ch>
2014-04-13 18:23:32 +0200

    Refactor list and show user specs

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -1,15 +1,14 @@
 require 'spec_helper'
 
-describe 'Listing users' do
+describe 'Showing users' do
   before { @user = create :user, :donald }
 
-  it 'displays users' do
-    visit users_path
+  it 'displays a user' do
+    visit user_path(@user)
 
     within dom_id_selector(@user) do
       expect(page).to have_content 'donald'
       expect(page).to have_content 'donald@example.com'
-      expect(page).to have_link 'Show'
     end
   end
 end
