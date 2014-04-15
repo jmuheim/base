bfb7d3691839ee69d86fbbfb14c83cf84588cb58
Joshua Muheim <joshua.muheim@sientia.ch>
2014-04-16 00:25:19 +0200

    Refactor actions I18n

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -1,14 +1,20 @@
 require 'spec_helper'
 
 describe 'Showing users' do
-  before { @user = create :user, :donald }
+  before do
+    @user = create :user, :donald
+    login_as(create :admin)
+  end
 
   it 'displays a user' do
     visit user_path(@user)
 
+    expect(page).to have_content 'donald'
     within dom_id_selector(@user) do
       expect(page).to have_content 'donald'
       expect(page).to have_content 'donald@example.com'
+      expect(page).to have_link 'Edit'
+      expect(page).to have_link 'Delete'
     end
   end
 end
