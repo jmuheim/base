8a3ef05d445481ec211566b3539c21bf71f79703
Joshua Muheim <joshua.muheim@sientia.ch>
2014-04-16 00:25:04 +0200

    Rename feature spec files to resemble action names

--- spec/features/users/index_spec.rb
+++ spec/features/users/index_spec.rb
@@ -1,15 +1,22 @@
 require 'spec_helper'
 
 describe 'Listing users' do
-  before { @user = create :user, :donald }
+  before do
+    @user = create :user, :donald
+    login_as(create :admin)
+  end
 
   it 'displays users' do
     visit users_path
 
+    expect(page).to have_content 'Users'
     within dom_id_selector(@user) do
       expect(page).to have_content 'donald'
       expect(page).to have_content 'donald@example.com'
       expect(page).to have_link 'Show'
+      expect(page).to have_link 'Edit'
+      expect(page).to have_link 'Delete'
     end
+    expect(page).to have_link 'Create User'
   end
 end
