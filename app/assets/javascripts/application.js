6e622daaf5761564d3485bfa2916a158e7c18727
Joshua Muheim <joshua.muheim@sientia.ch>
2014-05-07 12:32:58 +0200

    Refactor application.js/css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,21 +1,15 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
+// Don't change order of the first and last blocks (so turbolink plays nicely)!
+// Add more scripts to the block in the middle!
 //
 //= require jquery
-//= require jquery-ui
+//= require jquery.turbolinks
 //= require jquery-ujs
-//= require turbolinks
+//
 //= require bootstrap-sass
 //= require bootstrap-formhelpers
 //= require cocoon
 //= require dashboards
+//= require jquery-ui
+//
 //= require init
+//= require turbolinks
