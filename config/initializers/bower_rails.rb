a5ee3e349eb924a4eafb4be5b961284f2c9857b6
Joshua Muheim <joshua.muheim@sientia.ch>
2014-05-07 09:27:21 +0200

    Don't resolve relative paths before precompiling using bower-rails.
    See https://github.com/42dev/bower-rails/issues/79
    and https://github.com/jmuheim/base/pull/35
    
    Let's hope this works in production.

--- config/initializers/bower_rails.rb
+++ config/initializers/bower_rails.rb
@@ -1,3 +1,4 @@
 BowerRails.configure do |bower_rails|
-  bower_rails.resolve_before_precompile = true # Invokes rake bower:resolve before precompilation
+  # Don't use resolving relative paths for now, see https://github.com/jmuheim/base/pull/35 and https://github.com/42dev/bower-rails/issues/79.
+  # bower_rails.resolve_before_precompile = true # Invokes rake bower:resolve before precompilation
 end
