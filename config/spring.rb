72599ca2218f88a7485b613e31397b9b664cd5ca
Joshua Muheim <joshua.muheim@sientia.ch>
2014-05-09 10:18:49 +0200

    Don't manually watch factories (leads to double validation errors in user model)

--- config/spring.rb
+++ config/spring.rb
@@ -1,4 +1,2 @@
-Spring.watch 'spec/factories'
-
 # We can't use listen method at the moment, as guard isn't fully ready yet. See https://github.com/rails/spring/pull/194#issuecomment-42396462
 # Spring.watch_method = :listen
