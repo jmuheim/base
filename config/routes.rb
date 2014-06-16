8b03ad381baa17c4879a759c11a5a331cc47331b
Joshua Muheim <joshua.muheim@sientia.ch>
2014-06-16 17:11:53 +0200

    Use pages namespace, otherwise rails/info/routes doesn't work

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ Base::Application.routes.draw do
 
     root 'pages#home'
 
-    get '/*id', to: 'pages#show', # Keep this below root path definition (otherwise we have an infinite loop)!
-                as: 'page'
+    get '/pages/*id', to: 'pages#show', # Keep this below root path definition (otherwise we have an infinite loop)!
+                      as: 'page'
 
     # The priority is based upon order of creation: first created -> highest priority.
     # See how all your routes lay out with "rake routes".
