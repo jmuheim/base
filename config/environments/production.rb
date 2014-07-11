a8c34bb8a2fc89d694c6624725820edea1c0eb9a
Joshua Muheim <joshua.muheim@sientia.ch>
2014-07-11 13:52:18 +0200

    More trying to get glyphicons work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Base::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -60,8 +60,6 @@ Base::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-
-  config.assets.paths << "#{Rails}/vendor/assets/bootstrap"
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
