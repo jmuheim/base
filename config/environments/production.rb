f0886e24ffcb536a429c876cdc7d340f07e93a2b
Joshua Muheim <joshua.muheim@sientia.ch>
2014-07-11 13:43:29 +0200

    Try to make bootstrap icons work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,8 @@ Base::Application.configure do
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
 
+  config.assets.paths << "#{Rails}/vendor/assets/bootstrap"
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
