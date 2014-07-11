439762ee94faa5bf00db9c0fc29e7ed6c6083557
Joshua Muheim <joshua.muheim@sientia.ch>
2014-07-11 13:53:50 +0200

    Glyphicons

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module Base
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Berlin'
 
+    config.assets.paths << "#{Rails}/vendor/assets/bootstrap"
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
