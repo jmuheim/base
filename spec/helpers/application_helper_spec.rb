4fe9d077d8552f25d1222c712dadae7b665db796
Joshua Muheim <josh@muheimwebdesign.ch>
2015-03-24 11:23:35 +0100

    Refactor

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,29 +1,33 @@
 require 'rails_helper'
 
 describe ApplicationHelper do
-  describe '#icon(name)' do
-    subject { icon 'not-ok' }
+  describe '#icon' do
+    describe '#icon(name)' do
+      subject { icon 'not-ok' }
 
-    it { should have_css 'span.glyphicon.glyphicon-not-ok' }
-    it { should have_css 'span span.hide-text' }
-    it { should have_content 'Not Ok' }
+      it { should have_css 'span.glyphicon.glyphicon-not-ok' }
+      it { should have_css 'span span.hide-text' }
+      it { should have_content 'Not Ok' }
+    end
+
+    describe '#icon(name, content)' do
+      subject { icon 'not-ok', 'This is the content' }
+      it { should have_content 'This is the content' }
+    end
   end
 
-  describe '#icon(name, content)' do
-    subject { icon 'not-ok', 'This is the content' }
-    it { should have_content 'This is the content' }
-  end
+  describe '#container_for' do
+    describe '#container_for(object)' do
+      subject { container_for(user = create(:user)) { 'Some content' } }
 
-  describe '#container_for(object)' do
-    subject { container_for(user = create(:user)) { 'Some content' } }
+      it { should have_css 'div#user_1.user' }
+      it { should have_content 'Some content' }
+    end
 
-    it { should have_css 'div#user_1.user' }
-    it { should have_content 'Some content' }
-  end
+    describe '#container_for(object, :tag)' do
+      subject { container_for(create(:user), tag: 'article') { 'Some content' } }
 
-  describe '#container_for(object, :tag)' do
-    subject { container_for(create(:user), tag: 'article') { 'Some content' } }
-
-    it { should have_css 'article#user_1.user' }
+      it { should have_css 'article#user_1.user' }
+    end
   end
 end
