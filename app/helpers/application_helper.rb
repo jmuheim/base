344a20906fdd7577aeff93018c42d8be1ca9fda7
Joshua Muheim <josh@muheimwebdesign.ch>
2015-03-24 11:24:23 +0100

    Add page_title helper for uniform page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -66,4 +66,22 @@ module ApplicationHelper
       yield
     end
   end
+
+  def page_title(*args)
+    options = args.extract_options!
+    raise "You can't provide both a title and options!" if args[0] && options.any?
+    @page_title = args[0].nil? ? page_title_default(options) : args[0]
+  end
+
+  def page_title_or_flashes
+    if flash.any?
+      flash.map { |key, value| "#{t "flash.#{key}"}: #{value}" }.join
+    else
+      @page_title or raise 'No page title provided! Be sure to place a call to page_title somewhere in your view.'
+    end
+  end
+
+  def page_title_default(options = {})
+    t '.title', options
+  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -30,4 +30,58 @@ describe ApplicationHelper do
       it { should have_css 'article#user_1.user' }
     end
   end
+
+  describe '#page_title' do
+    # See http://stackoverflow.com/questions/29229368/rspec-rails-stub-virtual-path-for-translation-helper-to-test-an-application
+    # describe '#page_title' do
+    #   before { helper.instance_variable_set(:@virtual_path, 'pages.show.root') }
+    #   subject { page_title }
+    #
+    #   it { should eq 'Welcome to Base!' }
+    # end
+
+    describe '#page_title("Some title")' do
+      subject { page_title('Some title') }
+
+      it { should eq 'Some title' }
+    end
+
+    # See http://stackoverflow.com/questions/29229368/rspec-rails-stub-virtual-path-for-translation-helper-to-test-an-application
+    # describe '#page_title(user: "John")' do
+    #   before { helper.instance_variable_set(:@virtual_path, 'users.show') }
+    #   subject { page_title }
+    #
+    #   it { should eq 'User John' }
+    # end
+
+    describe '#page_title("New title", user: "John")' do
+      it 'raises an error when both a title and options are given' do
+        expect { page_title('New title', user: "John") }.to raise_error
+      end
+    end
+  end
+
+  describe '#page_title_or_flashes' do
+    it 'raises an error when page_title was not called before' do
+      expect { page_title_or_flashes }.to raise_error
+    end
+
+    # See http://stackoverflow.com/questions/29229368/rspec-rails-stub-virtual-path-for-translation-helper-to-test-an-application
+    # context 'without a flash' do
+    #   before { helper.instance_variable_set(:@virtual_path, 'pages.show.root') }
+    #   subject do
+    #     page_title
+    #     page_title_or_flashes
+    #   end
+    #
+    #   it { should eq 'Welcome to Base!' }
+    # end
+
+    context 'with a flash' do
+      before { controller.flash[:alert] = 'This is an alert!' }
+      subject { page_title_or_flashes }
+
+      it { should eq 'Alert: This is an alert!' }
+    end
+  end
 end
