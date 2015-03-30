3d0c548c8a3f3f599215b7d82464372ea264be06
Joshua Muheim <josh@muheimwebdesign.ch>
2015-03-30 15:55:28 +0200

    Refactor headline and title helper

--- app/helpers/page_title_and_headline_helper.rb
+++ app/helpers/page_title_and_headline_helper.rb
@@ -1,8 +1,14 @@
+# Helps managing uniform page title, headline, and flash messages.
 module PageTitleAndHeadlineHelper
-  def headline
-    @headline or raise 'No page heading provided! Be sure to call #headline_with_flash first.'
-  end
-
+  # Displays the current headline, followed by flash messages.
+  #
+  # Accepts parameter for customizing/replacing the headline, or simply looks up `.title` when no parameters given.
+  #
+  # Examples:
+  #
+  # - `headline_with_flash` simply translates `.title`
+  # - `headline_with_flash(key: 'value')` passes the provided keys to the translation of `.title`
+  # - `headline_with_flash('My title')` uses the given string (instead of translating `.title`)
   def headline_with_flash(*args)
     options = args.extract_options!
     heading = args[0]
@@ -11,10 +17,11 @@ module PageTitleAndHeadlineHelper
     @headline = heading.nil? ? default_headline(options) : heading
 
     content_tag :div, class: 'headline' do
-      content_tag(:h1, @headline) + render(partial: 'layouts/messages')
+      content_tag(:h1, @headline) + render(partial: 'layouts/flash')
     end
   end
 
+  # Generates a title tag, consisting of potential flash messages, the page's headline, and the app's name (except when on root path).
   def title_tag
     content_tag :title do
       parts = []
@@ -25,6 +32,12 @@ module PageTitleAndHeadlineHelper
     end
   end
 
+  # Returns the current headline (be sure to call #headline_with_flash first).
+  def headline
+    @headline or raise 'No page heading provided! Be sure to call #headline_with_flash first.'
+  end
+
+  # Returns the default headline (can be overriden in controllers).
   def default_headline(options = {})
     t '.title', options
   end
