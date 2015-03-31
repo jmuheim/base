8224b1359831b028b2697e6c5cdcc8f88b191f2f
Joshua Muheim <josh@muheimwebdesign.ch>
2015-03-31 14:53:25 +0200

    Update schema info

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,28 +4,27 @@
 # Table name: users
 #
 #  id                     :integer          not null, primary key
-#  name                   :string(255)
-#  email                  :string(255)
-#  encrypted_password     :string(255)
-#  reset_password_token   :string(255)
+#  name                   :string
+#  email                  :string
+#  encrypted_password     :string
+#  reset_password_token   :string
 #  reset_password_sent_at :datetime
 #  remember_created_at    :datetime
 #  sign_in_count          :integer          default(0), not null
 #  current_sign_in_at     :datetime
 #  last_sign_in_at        :datetime
-#  current_sign_in_ip     :string(255)
-#  last_sign_in_ip        :string(255)
-#  confirmation_token     :string(255)
+#  current_sign_in_ip     :string
+#  last_sign_in_ip        :string
+#  confirmation_token     :string
 #  confirmed_at           :datetime
 #  confirmation_sent_at   :datetime
-#  unconfirmed_email      :string(255)
+#  unconfirmed_email      :string
 #  failed_attempts        :integer          default(0), not null
-#  unlock_token           :string(255)
+#  unlock_token           :string
 #  locked_at              :datetime
 #  created_at             :datetime
 #  updated_at             :datetime
-#  guest                  :boolean          default(FALSE)
-#  avatar                 :string(255)
+#  avatar                 :string
 #
 # Indexes
 #
