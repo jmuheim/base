028b3d835c0d7b951be3233dd002209d5b1e2d18
Joshua Muheim <josh@muheimwebdesign.ch>
2015-03-31 18:42:12 +0200

    Fixes #45 (and adds some more specs for confirmation and password reset requests).

--- app/models/user.rb
+++ app/models/user.rb
@@ -53,12 +53,12 @@ class User < ActiveRecord::Base
   validates :avatar, file_size: {maximum: (Rails.env.test? ? 15 : 500).kilobytes.to_i} # TODO: It would be nice to stub the maximum within the spec itself. See https://gist.github.com/chrisbloom7/1009861#comment-1220820
 
   # https://github.com/plataformatec/devise/wiki/How-To:-Allow-users-to-sign-in-using-their-username-or-email-address
-  def self.find_first_by_auth_conditions(warden_conditions)
+  def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
-      where(conditions).where(['lower(name) = :value OR lower(email) = :value', { value: login.downcase }]).first
+      where(conditions.to_hash).where(["lower(name) = :value OR lower(email) = :value", { :value => login.downcase }]).first
     else
-      where(conditions).first
+      where(conditions.to_hash).first
     end
   end
 end
---
+++ spec/features/user/passwords/new_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'Requesting new password' do
+  before { @user = create :user, :donald }
+
+  it 'is possible to request a new password' do
+    visit new_user_password_path
+
+    within '#new_user' do
+      fill_in 'user_email', with: 'donald@example.com'
+      click_button 'Send me reset password instructions'
+    end
+
+    expect(page).to have_content 'You will receive an email with instructions on how to reset your password in a few minutes.'
+
+    visit_in_email('Change my password!', 'donald@example.com')
+
+    within '#new_user' do
+      fill_in 'user_password',              with: 'thenewpassword'
+      fill_in 'user_password_confirmation', with: 'thenewpassword'
+      click_button 'Change my password'
+    end
+
+    expect(page).to have_content 'Your password has been changed successfully. You are now signed in.'
+  end
+end
