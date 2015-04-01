0a9ac4dc881973a84902e5989b406cffded4ea3c
Joshua Muheim <josh@muheimwebdesign.ch>
2015-04-01 21:35:35 +0200

    Add specs (pending)

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
-  # TODO: load_and_authorize_resource
+  # Doesn't seem to work, see https://github.com/plataformatec/devise/issues/3349#issuecomment-88604326
+  # before_filter :authenticate_user!
+  # Or should it better be load_and_authorize_resource?
 
   def show
     @user = current_user
--- spec/features/user/registration/show_spec.rb
+++ spec/features/user/registration/show_spec.rb
@@ -1,18 +1,32 @@
 require 'rails_helper'
 
 describe 'Showing account' do
-  before do
-    @user = create :user, :donald, :with_avatar
-    login_as(@user)
-    visit user_registration_path
+  context 'as a guest' do
+    before { @user = create :user, :donald }
+
+    it 'does not grant permission to show the account' do
+      pending "before_filter :authenticate_user! doesn't seem to work, see https://github.com/plataformatec/devise/issues/3349#issuecomment-88604326"
+
+      visit user_registration_path
+
+      expect(page).to have_content 'You need to sign in or sign up before continuing.'
+    end
   end
 
-  it 'displays the account' do
-    within dom_id_selector(@user) do
-      expect(page).to have_content 'donald'
-      expect(page).to have_content 'donald@example.com'
-      expect(page).to have_css 'img[alt="donald"]'
-      expect(page).to have_link 'Edit'
+  context 'signed in as user' do
+    before do
+      @user = create :user, :donald, :with_avatar
+      login_as(@user)
+      visit user_registration_path
+    end
+
+    it 'displays the account' do
+      within dom_id_selector(@user) do
+        expect(page).to have_content 'donald'
+        expect(page).to have_content 'donald@example.com'
+        expect(page).to have_css 'img[alt="donald"]'
+        expect(page).to have_link 'Edit'
+      end
     end
   end
 end
