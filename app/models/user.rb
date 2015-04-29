2b8b596f09c9483f38aa0219fcc99739cd072d93
Joshua Muheim <josh@muheimwebdesign.ch>
2015-04-29 20:29:00 +0200

    rip hashrocket

--- app/models/user.rb
+++ app/models/user.rb
@@ -56,7 +56,7 @@ class User < ActiveRecord::Base
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
-      where(conditions.to_hash).where(["lower(name) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      where(conditions.to_hash).where(["lower(name) = :value OR lower(email) = :value", { value: login.downcase }]).first
     else
       where(conditions.to_hash).first
     end
