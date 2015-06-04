5178fc1d7ff75c6eec7bfef8fe84d222d652b51a
Joshua Muheim <josh@muheimwebdesign.ch>
2015-06-04 11:41:29 +0200

    Make markdown parse tables

--- spec/helpers/markdown_helper_spec.rb
+++ spec/helpers/markdown_helper_spec.rb
@@ -9,5 +9,9 @@ describe MarkdownHelper do
     it "doesn't blow up with nil as param" do
       expect(markdown(nil)).to eq ''
     end
+
+    it "converts php-markdown style tables" do
+      expect(markdown("| th1 | th2 |\n| --- | --- |\n| td1 | td2 |")).to have_css 'table'
+    end
   end
 end
