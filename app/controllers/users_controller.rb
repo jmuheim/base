6421140a22654dffc719d55f042021741154a664
Joshua Muheim <josh@muheimwebdesign.ch>
2015-06-29 11:34:09 +0200

    Remove not needed config

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :edit]
   load_and_authorize_resource
   inherit_resources
-  respond_to :html
 
   private
 
