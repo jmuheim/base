ff02229e78c72751038631c6e3ab4ba53f51d9a3
Joshua Muheim <josh@muheimwebdesign.ch>
2015-10-08 17:53:59 +0200

    Make sure that avatar uploads work correctly for registration process

--- spec/features/user/registration/edit_spec.rb
+++ spec/features/user/registration/edit_spec.rb
@@ -4,10 +4,11 @@ describe 'Editing account' do
   before do
     @user = create :user, :donald
     login_as(@user)
-    visit edit_user_registration_path
   end
 
   it 'edits the account' do
+    visit edit_user_registration_path
+
     expect(page).to have_active_navigation_items 'User menu', 'Edit account'
     expect(page).to have_breadcrumbs 'Base', 'Edit account'
     expect(page).to have_headline 'Edit account'
@@ -28,13 +29,99 @@ describe 'Editing account' do
       .and change { @user.avatar.to_s }
       .and change { @user.encrypted_password }
       .and change { @user.unconfirmed_email }.to('new-gustav@example.com')
+
+    expect(page).to have_flash 'You updated your account successfully, but we need to verify your new email address. Please check your email and follow the confirm link to confirm your new email address.'
   end
 
   it "doesn't change the password if left empty" do
+    visit edit_user_registration_path
+
     fill_in 'user_current_password', with: 's3cur3p@ssw0rd'
 
     expect {
       click_button 'Save'
     }.not_to change { @user.reload.encrypted_password }
+  end
+
+  # These specs make sure that the rather tricky image upload things are working as expected
+  describe 'avatar upload' do
+    it 'caches an uploaded avatar during validation errors' do
+      visit edit_user_registration_path
+
+      # Upload a file
+      attach_file 'user_avatar', dummy_file_path('image.jpg')
+
+      # Trigger validation error
+      click_button 'Save'
+      expect(page).to have_flash('User could not be updated.').of_type :alert
+
+      # Fill in current password
+      fill_in 'user_current_password', with: 's3cur3p@ssw0rd'
+
+      click_button 'Save'
+
+      expect(page).to have_flash 'Your account has been updated successfully.'
+      expect(File.basename(User.last.avatar.to_s)).to eq 'image.jpg'
+    end
+
+    it 'replaces a cached uploaded avatar with a new one after validation errors' do
+      visit edit_user_registration_path
+
+      # Upload a file
+      attach_file 'user_avatar', dummy_file_path('image.jpg')
+
+      # Trigger validation error
+      click_button 'Save'
+      expect(page).to have_flash('User could not be updated.').of_type :alert
+
+      # Upload another file
+      attach_file 'user_avatar', dummy_file_path('other_image.jpg')
+
+      # Fill in current password
+      fill_in 'user_current_password', with: 's3cur3p@ssw0rd'
+
+      click_button 'Save'
+
+      expect(page).to have_flash 'Your account has been updated successfully.'
+      expect(File.basename(User.last.avatar.to_s)).to eq 'other_image.jpg'
+    end
+
+    it 'allows to remove a cached uploaded avatar after validation errors' do
+      visit edit_user_registration_path
+
+      # Upload a file
+      attach_file 'user_avatar', dummy_file_path('image.jpg')
+
+      # Trigger validation error
+      click_button 'Save'
+      expect(page).to have_flash('User could not be updated.').of_type :alert
+
+      # Remove avatar
+      check 'user_remove_avatar'
+
+      # Fill in current password
+      fill_in 'user_current_password', with: 's3cur3p@ssw0rd'
+
+      click_button 'Save'
+
+      expect(page).to have_flash 'Your account has been updated successfully.'
+      expect(User.last.avatar.to_s).to eq ''
+    end
+
+    it 'allows to remove an uploaded avatar' do
+      @user.update_attributes! avatar: File.open(dummy_file_path('image.jpg'))
+
+      visit edit_user_registration_path
+      check 'user_remove_avatar'
+      fill_in 'user_current_password', with: 's3cur3p@ssw0rd'
+
+      expect {
+        click_button 'Save'
+
+        # Checking upon @user doesn't work, see https://github.com/carrierwaveuploader/carrierwave/issues/1752
+      }.to change { File.basename User.last.avatar.to_s }.from('image.jpg').to eq ''
+
+      expect(page).to have_flash 'Your account has been updated successfully.'
+    end
   end
 end
--- spec/features/user/registration/new_spec.rb
+++ spec/features/user/registration/new_spec.rb
@@ -17,11 +17,86 @@ describe 'Signing up' do
 
     click_button 'Sign up'
 
-    expect(page).to have_content 'You have signed up successfully.'
+    expect(page).to have_flash 'Welcome! You have signed up successfully.'
     expect(unread_emails_for('newuser@example.com').size).to eq 1
     expect(page).to have_link 'Log out'
 
     visit_in_email('Confirm my account', 'newuser@example.com')
-    expect(page).to have_content 'Your email address has been successfully confirmed.'
+    expect(page).to have_flash 'Your email address has been successfully confirmed.'
+  end
+
+  # These specs make sure that the rather tricky image upload things are working as expected
+  describe 'avatar upload' do
+    it 'caches an uploaded avatar during validation errors' do
+      visit new_user_registration_path
+
+      # Upload a file
+      attach_file 'user_avatar', dummy_file_path('image.jpg')
+
+      # Trigger validation error
+      click_button 'Sign up'
+      expect(page).to have_flash('User could not be created.').of_type :alert
+
+      # Fill in other needed fields
+      fill_in 'user_name',                  with: 'newuser'
+      fill_in 'user_email',                 with: 'newuser@example.com'
+      fill_in 'user_password',              with: 'somegreatpassword'
+      fill_in 'user_password_confirmation', with: 'somegreatpassword'
+
+      click_button 'Sign up'
+
+      expect(page).to have_flash 'Welcome! You have signed up successfully.'
+      expect(File.basename(User.last.avatar.to_s)).to eq 'image.jpg'
+    end
+
+    it 'replaces a cached uploaded avatar with a new one after validation errors' do
+      visit new_user_registration_path
+
+      # Upload a file
+      attach_file 'user_avatar', dummy_file_path('image.jpg')
+
+      # Trigger validation error
+      click_button 'Sign up'
+      expect(page).to have_flash('User could not be created.').of_type :alert
+
+      # Upload another file
+      attach_file 'user_avatar', dummy_file_path('other_image.jpg')
+
+      # Fill in other needed fields
+      fill_in 'user_name',                  with: 'newuser'
+      fill_in 'user_email',                 with: 'newuser@example.com'
+      fill_in 'user_password',              with: 'somegreatpassword'
+      fill_in 'user_password_confirmation', with: 'somegreatpassword'
+
+      click_button 'Sign up'
+
+      expect(page).to have_flash 'Welcome! You have signed up successfully.'
+      expect(File.basename(User.last.avatar.to_s)).to eq 'other_image.jpg'
+    end
+
+    it 'allows to remove a cached uploaded avatar after validation errors' do
+      visit new_user_registration_path
+
+      # Upload a file
+      attach_file 'user_avatar', dummy_file_path('image.jpg')
+
+      # Trigger validation error
+      click_button 'Sign up'
+      expect(page).to have_flash('User could not be created.').of_type :alert
+
+      # Remove avatar
+      check 'user_remove_avatar'
+
+      # Fill in other needed fields
+      fill_in 'user_name',                  with: 'newuser'
+      fill_in 'user_email',                 with: 'newuser@example.com'
+      fill_in 'user_password',              with: 'somegreatpassword'
+      fill_in 'user_password_confirmation', with: 'somegreatpassword'
+
+      click_button 'Sign up'
+
+      expect(page).to have_flash 'Welcome! You have signed up successfully.'
+      expect(User.last.avatar.to_s).to eq ''
+    end
   end
 end
