e062daa2691fc20c964c86c4edd43801edf049ae
Joshua Muheim <josh@muheimwebdesign.ch>
2015-10-09 10:40:12 +0200

    Add todo code

--- spec/features/users/edit_spec.rb
+++ spec/features/users/edit_spec.rb
@@ -48,5 +48,86 @@ describe 'Editing user' do
 
       expect(page).to have_flash 'User was successfully updated.'
     end
+
+    # describe 'avatar upload' do
+    #   it 'caches an uploaded avatar during validation errors' do
+    #     visit edit_user_registration_path
+    #
+    #     # Upload a file
+    #     attach_file 'user_avatar', dummy_file_path('image.jpg')
+    #
+    #     # Trigger validation error
+    #     click_button 'Save'
+    #     expect(page).to have_flash('User could not be updated.').of_type :alert
+    #
+    #     # Fill in current password
+    #     fill_in 'user_current_password', with: 's3cur3p@ssw0rd'
+    #
+    #     click_button 'Save'
+    #
+    #     expect(page).to have_flash 'Your account has been updated successfully.'
+    #     expect(File.basename(User.last.avatar.to_s)).to eq 'image.jpg'
+    #   end
+    #
+    #   it 'replaces a cached uploaded avatar with a new one after validation errors' do
+    #     visit edit_user_registration_path
+    #
+    #     # Upload a file
+    #     attach_file 'user_avatar', dummy_file_path('image.jpg')
+    #
+    #     # Trigger validation error
+    #     click_button 'Save'
+    #     expect(page).to have_flash('User could not be updated.').of_type :alert
+    #
+    #     # Upload another file
+    #     attach_file 'user_avatar', dummy_file_path('other_image.jpg')
+    #
+    #     # Fill in current password
+    #     fill_in 'user_current_password', with: 's3cur3p@ssw0rd'
+    #
+    #     click_button 'Save'
+    #
+    #     expect(page).to have_flash 'Your account has been updated successfully.'
+    #     expect(File.basename(User.last.avatar.to_s)).to eq 'other_image.jpg'
+    #   end
+    #
+    #   it 'allows to remove a cached uploaded avatar after validation errors' do
+    #     visit edit_user_registration_path
+    #
+    #     # Upload a file
+    #     attach_file 'user_avatar', dummy_file_path('image.jpg')
+    #
+    #     # Trigger validation error
+    #     click_button 'Save'
+    #     expect(page).to have_flash('User could not be updated.').of_type :alert
+    #
+    #     # Remove avatar
+    #     check 'user_remove_avatar'
+    #
+    #     # Fill in current password
+    #     fill_in 'user_current_password', with: 's3cur3p@ssw0rd'
+    #
+    #     click_button 'Save'
+    #
+    #     expect(page).to have_flash 'Your account has been updated successfully.'
+    #     expect(User.last.avatar.to_s).to eq ''
+    #   end
+    #
+    #   it 'allows to remove an uploaded avatar' do
+    #     @user.update_attributes! avatar: File.open(dummy_file_path('image.jpg'))
+    #
+    #     visit edit_user_registration_path
+    #     check 'user_remove_avatar'
+    #     fill_in 'user_current_password', with: 's3cur3p@ssw0rd'
+    #
+    #     expect {
+    #       click_button 'Save'
+    #
+    #       # Checking upon @user doesn't work, see https://github.com/carrierwaveuploader/carrierwave/issues/1752
+    #     }.to change { File.basename User.last.avatar.to_s }.from('image.jpg').to eq ''
+    #
+    #     expect(page).to have_flash 'Your account has been updated successfully.'
+    #   end
+    # end
   end
 end
