8ee1850234d2569e5e295dd57f9b4657974cbd9c
Joshua Muheim <josh@josh.ch>
2016-01-21 17:52:59 +0100

    Refactor spec

--- spec/features/users/index_spec.rb
+++ spec/features/users/index_spec.rb
@@ -25,21 +25,27 @@ describe 'Listing users' do
   end
 
   it 'allows to filter users' do
-    create :user, name: 'anne'
-    create :user, name: 'marianne'
-    create :user, name: 'eva'
+    @user_1 = create :user, name: 'anne'
+    @user_2 = create :user, name: 'marianne'
+    @user_3 = create :user, name: 'eva'
 
     visit users_path
 
-    expect(all('.users tbody tr').count).to eq 5
+    expect(page).to have_css dom_id_selector(@user_1)
+    expect(page).to have_css dom_id_selector(@user_2)
+    expect(page).to have_css dom_id_selector(@user_3)
 
     fill_in 'q_name_cont', with: 'anne'
     click_button 'Filter'
 
-    expect(all('.users tbody tr').count).to eq 2
+    expect(page).to     have_css dom_id_selector(@user_1)
+    expect(page).to     have_css dom_id_selector(@user_2)
+    expect(page).not_to have_css dom_id_selector(@user_3)
 
     click_link 'Remove filter'
 
-    expect(all('.users tbody tr').count).to eq 5
+    expect(page).to have_css dom_id_selector(@user_1)
+    expect(page).to have_css dom_id_selector(@user_2)
+    expect(page).to have_css dom_id_selector(@user_3)
   end
 end
