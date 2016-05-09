19ca360652c0735e97d3c0e9b6a39bcd96445521
Joshua Muheim <josh@josh.ch>
2016-05-09 16:27:23 +0200

    Add tilt/pandoc

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require 'active_record/railtie'
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'sprockets/railtie'
-require 'tilt/erb'
+require 'tilt/pandoc'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
