6ee4e196f81ed265d0037ea81f4b14e2ba722bb2
Joshua Muheim <josh@josh.ch>
2017-01-05 17:35:05 +0100

    Refactor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,54 @@
 require 'update_lock'
 
 class UsersController < ApplicationController
-  before_filter :authenticate_user!, only: [:new, :edit]
   load_and_authorize_resource
-  include UpdateLock
+  before_filter :authenticate_user!, only: [:new, :edit]
   before_filter :add_base_breadcrumbs
+
+  rescue_from ActiveRecord::StaleObjectError do
+    prepare_conflicted_stuff(@user)
+  end
 
   def index
     @q = @users.ransack(params[:q])
     @users = @q.result(distinct: true)
   end
 
+  def create
+    if @user.save
+      redirect_to @user, notice: t('flash.actions.create.notice', resource_name: User.model_name.human)
+    else
+      flash.now[:alert] = t('flash.actions.create.alert', resource_name: User.model_name.human)
+      render :new
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: t('flash.actions.update.notice', resource_name: User.model_name.human)
+    else
+      flash.now[:alert] = t('flash.actions.update.alert', resource_name: User.model_name.human)
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_url, notice: t('flash.actions.destroy.notice', resource_name: User.model_name.human)
+  end
+
   private
 
-  def permitted_params
-    params.permit(user: [:name,
-                         :email,
-                         :avatar,
-                         :avatar_cache,
-                         :remove_avatar,
-                         :about,
-                         :password,
-                         :password_confirmation,
-                         :lock_version
-                       ])
+  def user_params
+    params.require(:user).permit(:name,
+                                 :email,
+                                 :avatar,
+                                 :avatar_cache,
+                                 :remove_avatar,
+                                 :about,
+                                 :password,
+                                 :password_confirmation,
+                                 :lock_version)
   end
 
   def add_base_breadcrumbs
@@ -37,5 +62,23 @@ class UsersController < ApplicationController
     if ['new', 'create'].include? action_name
       @last_breadcrumb = t 'actions.new'
     end
+  end
+
+  def prepare_conflicted_stuff(resource)
+    flash.now[:alert] = t(
+      'flash.actions.update.stale',
+      resource_name: resource.class.model_name.human,
+      stale_info:    t('flash.actions.update.stale_attributes_list',
+        stale_attributes: resource.stale_info.map { |info| info.human_attribute_name }.to_sentence,
+        count:            resource.stale_info.size
+      )
+    )
+
+    # Set lock version to current value in DB so when saving again (after manually solving the conflicts), no StaleObjectError is thrown anymore
+    resource.stale_info.each do |stale_info|
+      stale_info.resource.lock_version = stale_info.resource.class.find(stale_info.resource.id).lock_version
+    end
+
+    render :edit, status: :conflict
   end
 end
