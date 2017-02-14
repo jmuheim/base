1a677e2ef9c5744730b4c21e81e6e3d71de71d3b
Joshua Muheim <josh@josh.ch>
2017-02-14 19:49:17 +0100

    Refactor menu

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,9 @@
 class Page < ApplicationRecord
   scope :appear_in_navigation, -> { where(appear_in_navigation: true) }
 
+  validates :title, presence: true
+  validates :content, presence: true
+
   def navigation_title
     super.blank? ? title : super
   end
