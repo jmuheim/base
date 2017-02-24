8987dfd1abcce202edd9845c04118f729e9167be
Joshua Muheim <josh@josh.ch>
2017-02-24 17:45:32 +0100

    Fix schema

--- db/migrate/20170217183844_create_images.rb
+++ db/migrate/20170217183844_create_images.rb
@@ -4,7 +4,7 @@ class CreateImages < ActiveRecord::Migration[5.0]
       t.string     :file
       t.references :page
       t.string     :identifier # Although the identifier always is an integer, it can't be type :number because it exceeds its max. limit (11)
-      t.integer    :lock_version, :integer, default: 0
+      t.integer    :lock_version, default: 0
 
       t.timestamps null: false
     end
