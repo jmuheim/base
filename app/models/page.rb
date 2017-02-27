83c3ec0f4c0b8ea8a057771a33e1368d498d297b
Joshua Muheim <josh@josh.ch>
2017-02-27 08:57:43 +0100

    Asdf

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,4 +3,15 @@ class Page < ApplicationRecord
   accepts_pasted_images_for :content, :notes
 
   validates :title, presence: true
+
+  def content_with_page_references
+    content_with_referenced_images.lines.map do |line|
+      line.gsub!(/\[(.*?)\]\((.*?)\)/) do
+        page = Page.find $2
+        "![#{$1 || page.title}](#{link_to page})"
+      end
+
+      line
+    end.join
+  end
 end
