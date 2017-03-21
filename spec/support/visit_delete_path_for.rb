745f103b022df46b288648dbf65bc9bd6357d265
Joshua Muheim <josh@josh.ch>
2017-03-21 21:06:06 +0100

    Make delete specs possible for nested resources

--- spec/support/visit_delete_path_for.rb
+++ spec/support/visit_delete_path_for.rb
@@ -1,14 +1,14 @@
 module FeatureSpecVisitDeletePathHelper
-  def visit_delete_path_for(resource, resource_path = nil)
-    raise "Resource must not be nil" if resource.nil?
-    resource_path ||= "#{resource.resource_class}_path"
+  def visit_delete_path_for(resources, resource_path = nil)
+    resources = [resources] unless resources.is_a?(Array)
+    resource_path ||= "#{resources.map { |resource| resource.class.to_s.underscore }.join('_')}_path"
 
     # Send delete request using capybara, see http://makandracards.com/makandra/18023-trigger-a-delete-request-with-capybara
     case page.driver
     when Capybara::RackTest::Driver
-      page.driver.submit :delete, send(resource_path, resource), {}
+      page.driver.submit :delete, send(resource_path, *resources), {}
     else # e.g. Capybara::Selenium::Driver
-      visit send(resource_path, resource, method: :delete)
+      visit send(resource_path, resources, method: :delete)
     end
   end
 end
