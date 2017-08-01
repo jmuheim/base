69bbf4598ec12166ca29e0967c1fbb86c4c61bb7
Joshua Muheim <josh@josh.ch>
2017-08-01 13:05:03 +0200

    Fix spec

--- spec/features/user/passwords/new_spec.rb
+++ spec/features/user/passwords/new_spec.rb
@@ -6,7 +6,7 @@ describe 'Requesting new password' do
   it 'is possible to request a new password' do
     visit new_user_password_path
 
-    expect(page).to have_title 'Forgot your password? - Base'
+    expect(page).to have_title 'Forgot your password? - Project Manager'
     expect(page).to have_active_navigation_items 'Sign in'
     expect(page).to have_breadcrumbs 'Base', 'Sign in', 'Forgot your password?'
     expect(page).to have_headline 'Forgot your password?'
