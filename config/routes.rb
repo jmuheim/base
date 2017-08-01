d1a9dff74edd61b5add5790361173519ceba5486
Joshua Muheim <josh@josh.ch>
2017-08-01 15:17:03 +0200

    Add project views

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
 
     resources :users
     resources :pages
+    resources :projects
 
     [403, 404, 422, 500].each do |code|
       get code, to: 'errors#show', code: code
