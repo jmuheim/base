4ecd7e28d015fde44112f591514385f65608073d
Reto Inniger <reto.inniger@gmx.ch>
2017-09-18 18:10:41 +0200

    show fix

--- app/models/timetrack.rb
+++ app/models/timetrack.rb
@@ -2,5 +2,6 @@ class Timetrack < ApplicationRecord
   has_paper_trail only: [:name, :description, :work_time, :bill_time]
 
   validates :name, presence: true
-  validates :work_time, presence: true
+  validates :work_time, presence: true, numericality: { greater_than: 0 }
+  validates :bill_time, numericality: { greater_than: 0 }
 end
