534fd4dc240e2fb5c4f4a9e0cb56c3b2a6f5a78c
Reto Inniger <reto.inniger@gmx.ch>
2017-09-19 14:59:48 +0200

    spec not fix

--- spec/models/timetrack_spec.rb
+++ spec/models/timetrack_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Timetrack, type: :model do
   it { should validate_presence_of(:name).with_message "can't be blank"}
-  it { should validate_presence_of(:work_time).with_message "can't be blank" }
-  it { should validate_presence_of(:bill_time).with_message "can't be blank" }
-  pending 'Numericality of work time, incl. greater 0!'
-  pending 'Numericality of bill time, incl. greater 0 and less or equal to work time!'
+  it { should validate_presence_of(:work_time).with_message "can't be blank", "is not a number" }
+  it { should validate_presence_of(:bill_time).with_message "is not a number" }
+  it { should validate_numericality_of(:work_time).is_greater_than_or_equal_to(0).with_message "must be greater than 0" }
+  #it { should validate_numericality_of(:bill_time).is_less_than_or_equal_to(:work_time).with_message "is not a number"}
 
   it 'has a valid factory' do
     expect(create(:timetrack)).to be_valid
