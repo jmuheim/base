2899fb452c2f7e769b893b879c87eeb9c3604276
Reto Inniger <reto.inniger@gmx.ch>
2017-09-21 20:53:09 +0200

    spec timetrack first fix

--- spec/models/timetrack_spec.rb
+++ spec/models/timetrack_spec.rb
@@ -4,12 +4,22 @@ RSpec.describe Timetrack, type: :model do
   it { should validate_presence_of(:name).with_message "can't be blank"}
   it { should validate_presence_of(:work_time).with_message "can't be blank" }
   it { should validate_numericality_of(:work_time).with_message "is not a number" }
-  #it { should validate_numericality_of(:work_time).is_greater_than_or_equal_to(0).with_message "must be greater than or equal 0" }
-  #it { should validate_numericality_of(:bill_time).with_message "is not a number" }
-  #it { should validate_numericality_of(:bill_time).is_less_than_or_equal_to(:work_time).with_message "must be less than or equal to"}
 
   it 'has a valid factory' do
     expect(create(:timetrack)).to be_valid
+  end
+
+  describe 'validates numericality of bill time' do
+    it 'validates when a work time available' do
+      timetrack = create :timetrack, work_time: 2
+      expect(timetrack.valid?).to be_truthy
+      expect(timetrack.errors).to be_truthy
+    end
+
+    it "doesn't validate when no work time available" do
+      timetrack = create :timetrack
+
+    end
   end
 
   describe 'versioning', versioning: true do
