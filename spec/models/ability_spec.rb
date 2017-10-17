e2dc56a15e580d867c591be8b56499c67411ef3d
Reto Inniger <reto.inniger@gmx.ch>
2017-10-17 18:20:31 +0200

    ability spec

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -6,6 +6,9 @@ require 'rails_helper'
 describe Ability do
   context 'when is a guest' do
     subject { Ability.new nil }
+    before  { @project = create(:project) }
+    before  { @customer = create(:customer) }
+    before  { @timetrack = create(:timetrack) }
 
     describe 'managing pages' do
       it { should_not be_able_to(:create, Page) }
@@ -36,10 +39,34 @@ describe Ability do
 
       it { should_not be_able_to(:destroy, User.new) }
     end
+
+    describe 'managing customer' do
+      it { should_not be_able_to(:read, @customer)}
+      it { should_not be_able_to(:create, @customer) }
+      it { should_not be_able_to(:destroy, @customer) }
+      it { should_not be_able_to(:update, @customer) }
+    end
+
+    describe 'managing project' do
+      it { should_not be_able_to(:read, @project)}
+      it { should_not be_able_to(:create, @project) }
+      it { should_not be_able_to(:destroy, @project) }
+      it { should_not be_able_to(:update, @project) }
+    end
+
+    describe 'managing timetrack' do
+      it { should_not be_able_to(:read, @timetrack)}
+      it { should_not be_able_to(:create, @timetrack) }
+      it { should_not be_able_to(:destroy, @timetrack) }
+      it { should_not be_able_to(:update, @timetrack) }
+    end
   end
 
   context 'when is a registered user' do
     before  { @user = create(:user) }
+    before  { @project = create(:project) }
+    before  { @customer = create(:customer) }
+    before  { @timetrack = create(:timetrack) }
     subject { Ability.new(@user) }
 
     describe 'managing pages' do
@@ -73,10 +100,34 @@ describe Ability do
       it { should_not be_able_to(:destroy, User.new) }
       it { should_not be_able_to(:destroy, @user) }
     end
+
+    describe 'managing customer' do
+      it { should be_able_to(:read, @customer)}
+      it { should be_able_to(:create, @customer) }
+      it { should be_able_to(:destroy, @customer) }
+      it { should be_able_to(:update, @customer) }
+    end
+
+    describe 'managing project' do
+      it { should be_able_to(:read, @project)}
+      it { should be_able_to(:create, @project) }
+      it { should be_able_to(:destroy, @project) }
+      it { should be_able_to(:update, @project) }
+    end
+
+    describe 'managing timetrack' do
+      it { should be_able_to(:read, @timetrack)}
+      it { should be_able_to(:create, @timetrack) }
+      it { should be_able_to(:destroy, @timetrack) }
+      it { should be_able_to(:update, @timetrack) }
+    end
   end
 
   context 'when is an admin' do
     before  { @admin = create(:admin) }
+    before  { @project = create(:project) }
+    before  { @customer = create(:customer) }
+    before  { @timetrack = create(:timetrack) }
     subject { Ability.new(@admin) }
 
     it { should be_able_to(:access, :rails_admin) }
@@ -111,6 +162,27 @@ describe Ability do
 
       it { should     be_able_to(:destroy, User.new) }
       it { should_not be_able_to(:destroy, @admin) }
+    end
+
+    describe 'managing customer' do
+      it { should be_able_to(:read, @customer)}
+      it { should be_able_to(:create, @customer) }
+      it { should be_able_to(:destroy, @customer) }
+      it { should be_able_to(:update, @customer) }
+    end
+
+    describe 'managing project' do
+      it { should be_able_to(:read, @project)}
+      it { should be_able_to(:create, @project) }
+      it { should be_able_to(:destroy, @project) }
+      it { should be_able_to(:update, @project) }
+    end
+
+    describe 'managing timetrack' do
+      it { should be_able_to(:read, @timetrack)}
+      it { should be_able_to(:create, @timetrack) }
+      it { should be_able_to(:destroy, @timetrack) }
+      it { should be_able_to(:update, @timetrack) }
     end
   end
 end
