9d84b2f2227a42ce3b70ef8fa8649efefd764a4e
Joshua Muheim <josh@josh.ch>
2017-11-17 17:52:09 +0100

    Fix

--- spec/controllers/homepage_controller_spec.rb
+++ spec/controllers/homepage_controller_spec.rb
@@ -1,20 +1,15 @@
 require 'rails_helper'
 
-describe ApplicationController do
-  controller(ApplicationController) do
-    def index
-      render body: 'Hello World'
-    end
-  end
-
+describe HomepageController do
+  # This is vicarious for any sub controller of ApplicationController
   describe 'locale parameter' do
     it 'is set to english when not available in the request' do
-      get :index
+      get :show
       expect(I18n.locale).to eq :en
     end
 
     it 'can be set through the request' do
-      get :index, params: {locale: :de}
+      get :show, params: {locale: :de}
       expect(I18n.locale).to eq :de
     end
   end
