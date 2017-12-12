f361422a5ea65a5f7dba4c276a31102ce6ef94a1
Joshua Muheim <josh@josh.ch>
2017-12-12 14:15:53 +0100

    Fix autocomplete input

--- app/inputs/autocomplete_input.rb
+++ app/inputs/autocomplete_input.rb
@@ -2,11 +2,11 @@ class AutocompleteInput < StringInput
   def input_type
     :text
   end
-  
+
   def collection
     options[:collection]
   end
-  
+
   def input(wrapper_options)
     help_blocks = []
 
@@ -20,23 +20,46 @@ class AutocompleteInput < StringInput
 
     associated_object = object.send(attribute_name)
     value = nil
-    
-    filter_id = 'page_parent_id_filter' # TODO: Ausprogrammieren!
+
+    filter_id = "#{object.class.to_s.underscore}_#{label_target}_id_filter"
     super(wrapper_options.merge(value: value, name: nil, id: filter_id)) + template.content_tag(:span, nil, class: [:fa, :arrow]) + autocomplete_collection + help_block
   end
-  
+
   def autocomplete_collection
+    collection_options = {
+      as: :radio_buttons,
+      label: t('simple_form.inputs.autocomplete.options_for', attribute: label_translation),
+      collection: collection,
+      wrapper_html: {hidden: true}
+    }
+
+    collection_options[:label_method] = options[:label_method] if options[:label_method]
+
     # TODO: include_blank option! At the time being, only with esc key it is possible to reset to no option!
+    # if options[:label_method]
+    #   collection_options[:collection] = if options[:include_blank]
+    #                                       blank_option = Object.new
+    #                                       blank_option.define_singleton_method collection_options[:label_method] do
+    #                                          I18n.t('simple_form.inputs.autocomplete.number_in_total')
+    #                                        end
+    #
+    #                                       [blank_option] + collection
+    #                                     else
+    #                                       collection
+    #                                     end
+    # end
+
     template.content_tag :div, class: 'relative-wrapper' do
-      @builder.association(attribute_name, as: :radio_buttons, label: t('simple_form.inputs.autocomplete.options_for', attribute: label_translation), collection: collection, label_method: options[:label_method], wrapper_html: {hidden: true})
+      @builder.association(attribute_name, collection_options)
     end
   end
-  
+
   def options
     adg_options = {
       numberInTotalText:  t('simple_form.inputs.autocomplete.number_in_total'),
       numberFilteredText: t('simple_form.inputs.autocomplete.number_filtered')
     }
+
     merge_wrapper_options(super, wrapper_html: {data: {'adg-autocomplete': adg_options.to_json}})
   end
 end
