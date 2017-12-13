697c47088eae476a205e25d6a767acfcf6c87f03
Joshua Muheim <josh@josh.ch>
2017-12-13 16:26:44 +0100

    Add schema version

--- db/migrate/20131224105939_init_schema.rb
+++ db/migrate/20131224105939_init_schema.rb
@@ -1,4 +1,4 @@
-class InitSchema < ActiveRecord::Migration
+class InitSchema < ActiveRecord::Migration[5.0]
   def up
 
     create_table "images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
