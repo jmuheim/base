301cfb321e2dbbf851eed2170d8a52e6d94f93bc
Joshua Muheim <josh@josh.ch>
2018-06-08 14:48:54 +0200

    Navbar optimisations

--- app/helpers/navbar_helper.rb
+++ app/helpers/navbar_helper.rb
@@ -12,7 +12,7 @@ class Lego < Struct.new(:id, :view, :options, :block)
     self.view = view
     self.options = options
 
-    prepare_defaults
+    options.merge! defaults
 
     super
   end
@@ -27,26 +27,68 @@ class Lego < Struct.new(:id, :view, :options, :block)
                                      }.merge(options)
   end
 
-  # Override in sub class if needed
-  define_method :prepare_defaults { }
+  def defaults
+    {}
+  end
 end
 
 class Navbar < Lego
-  def prepare_defaults
-    self.options[:klass] ||= ['navbar-dark', 'bg-dark', 'navbar-expand-md']
+  def defaults
+    { home: nil,
+      klass: ['navbar-dark', 'bg-dark', 'navbar-expand-md'],
+      collapse_target: "#{id}_collapse"
+    }
   end
 
-  def item(target, options = {}, &block)
-    options[:target] = target
-
-    Item.new(view, options, block).render
+  def home(&block)
+    options[:home] = view.capture(self, &block)
   end
 
-  class Item < Lego
-    attr_accessor :target
+  def nav(id, options = {}, &block)
+    Nav.new(id, view, options, block).render
+  end
 
-    def prepare_defaults
-      self.target = options[:target]
+  class Nav < Lego
+    def item(target, options = {}, &block)
+      options[:target] = target
+
+      Item.new(view, options, block).render
+    end
+
+    def dropdown(title, options = {}, &block)
+      options[:title] = title
+
+      Dropdown.new(title, view, options, block).render
+    end
+
+    class Item < Lego
+      attr_accessor :target
+
+      def defaults
+        { target: options[:target] }
+      end
+    end
+
+    class Dropdown < Lego
+      attr_accessor :title
+
+      def defaults
+        { title: options[:title] }
+      end
+
+      def item(target, options = {}, &block)
+        options[:target] = target
+
+        Item.new(view, options, block).render
+      end
+
+      class Item < Lego # Duplicate? Smelly?
+        attr_accessor :target
+
+        def defaults
+          { target: options[:target] }
+        end
+      end
     end
   end
 end
