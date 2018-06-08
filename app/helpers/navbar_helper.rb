51426e60aaa2795f0197d239bb8c5db6ac19ab78
Joshua Muheim <josh@josh.ch>
2018-06-08 13:15:26 +0200

    Refactored to lego pattern ;-)

--- app/helpers/navbar_helper.rb
+++ app/helpers/navbar_helper.rb
@@ -1,56 +1,52 @@
 module NavbarHelper
   def navbar(id, options = {}, &block)
-    Navbar.new(id, options, self, block).render
+    Navbar.new(id, self, options, block).render
+  end
+end
+
+class Lego < Struct.new(:id, :view, :options, :block)
+  delegate :link_to, to: :view
+
+  def initialize(id = nil, view, options, block)
+    self.id = "#{id}_#{name}"
+    self.view = view
+    self.options = options
+
+    prepare_defaults
+
+    super
   end
 
-  class Navbar < Struct.new(:id, :options, :view, :callback)
-    delegate :link_to, to: :view
-    attr_accessor :navbar_options, :options
+  def name
+    self.class.name.underscore
+  end
 
-    def initialize(id, options, view, callback)
-      self.options = options
+  def render
+    view.render "bootstrap/#{name}", { id:      id,
+                                       content: view.capture(self, &block) # TODO: Would be nicer to use yield in view!
+                                     }.merge(options)
+  end
 
-      self.navbar_options = {
-        container_tag: options[:container_tag] || 'div'
-      }
+  # Override in sub class if needed
+  define_method :prepare_defaults { }
+end
 
-      self.id = "#{id}_navbar"
+class Navbar < Lego
+  def prepare_defaults
+    self.options[:klass] ||= ['navbar-dark', 'bg-dark', 'navbar-expand-md']
+  end
 
-      super
-    end
+  def item(target, options = {}, &block)
+    options[:target] = target
 
-    def render
-      view.render 'bootstrap/navbar', id:   id,
-                                         body: view.capture(self, &callback),
-                                         container_tag: navbar_options[:container_tag]
-    end
+    Item.new(view, options, block).render
+  end
 
-    def item(target, &block)
-      Item.new(target, options[:item_options] || {}, view, block).render
-    end
+  class Item < Lego
+    attr_accessor :target
 
-    class Item < Struct.new(:target, :options, :view, :callback)
-      attr_accessor :title_content, :body_content, :item_options
-
-      def initialize(target, options, view, block)
-        self.target = target
-
-        self.item_options = {
-          title_tag:   options[:title_tag]   || 'h3',
-          item_class: options[:item_class] || nil # e.g. bg-light
-        }
-
-        super
-      end
-
-      def render
-        view.capture(self, &callback)
-        view.render 'bootstrap/navbar/item', target: target,
-                                                title:       title_content,
-                                                 body:        body_content,
-                                                 title_tag:   item_options[:title_tag],
-                                                 item_class: item_options[:item_class]
-      end
+    def prepare_defaults
+      self.target = options[:target]
     end
   end
 end
