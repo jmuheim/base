ced77f90153bdaca968f03d5dfd5b3002fb8b9b5
Joshua Muheim <josh@josh.ch>
2019-09-24 14:41:59 +0200

    More

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -3,7 +3,9 @@
 # The reason is that transactions don't reset the auto increment value of tables (see http://stackoverflow.com/questions/11996522). Although it's a bit smelly, we rely on hard coded IDs in specs quite heavily, as it's much easier in some cases.
 RSpec.configure do |config|
   config.around(:each) do |example|
-    DatabaseCleaner.strategy = :truncation
+    # See https://github.com/DatabaseCleaner/database_cleaner/issues/445
+    DatabaseCleaner.clean_with :truncation, except: %w(ar_internal_metadata schema_migrations)
+
     DatabaseCleaner.cleaning do
       example.run
     end
