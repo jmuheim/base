fa5fac6911fb58670fbd9f04e4e99f2c8d0a3ca7
Joshua Muheim <josh@josh.ch>
2019-10-17 11:37:00 +0200

    Test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.public_file_server.enabled = false
+  config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
