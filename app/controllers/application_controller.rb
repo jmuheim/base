66cdd4c32fa4b54f0a7917b75bfa03d737e8e531
Joshua Muheim <josh@josh.ch>
2020-02-26 11:17:03 +0100

    Only ask for HTTP auth on production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 require 'application_responder'
 
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: 'admin', password: Rails.application.secrets.http_auth_password if Rails.env.production?
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
