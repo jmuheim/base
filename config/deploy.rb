98777789883097ac48dfa3d8c75daff1190dbefc
Joshua Muheim <josh@josh.ch>
2020-03-28 15:50:27 +0100

    Lock cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.2"
+lock "~> 3.12.2"
 
 set :application, "my_app_name"
 set :repo_url, "git@example.com:me/my_repo.git"
