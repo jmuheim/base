2901c77c5f5c1ea00155e4d64fe0d21608109d88
Joshua Muheim <josh@josh.ch>
2020-12-10 20:36:29 +0100

    More

--- db/migrate/20201210093351_create_app_configs.rb
+++ db/migrate/20201210093351_create_app_configs.rb
@@ -4,14 +4,14 @@ class CreateAppConfigs < ActiveRecord::Migration[5.2]
     create_table :app_configs, id: false do |t|
       t.integer :id, null: false, primary_key: true, default: 1, index: {unique: true}
       t.string :app_abbreviation, null: false
-      t.string :app_name, null: false
-      t.string :app_slogan_de, null: false
-      t.string :app_slogan_en, null: false
-      t.string :organisation_name_de, null: false
-      t.string :organisation_name_en, null: false
-      t.string :organisation_abbreviation_de, null: false
-      t.string :organisation_abbreviation_en, null: false
-      t.string :organisation_url, null: false
+      t.string :app_name
+      t.string :app_slogan_de
+      t.string :app_slogan_en
+      t.string :organisation_name_de
+      t.string :organisation_name_en
+      t.string :organisation_abbreviation_de
+      t.string :organisation_abbreviation_en
+      t.string :organisation_url
 
       t.integer :lock_version, default: 0, null: false
 
