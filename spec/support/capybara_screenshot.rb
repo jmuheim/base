8074c1f15837aef27c987026fb4f902e3591de78
Joshua Muheim <josh@josh.ch>
2020-12-11 18:11:18 +0100

    Remove legacy stuff

--- spec/support/capybara_screenshot.rb
+++
@@ -1,4 +0,0 @@
-# See https://github.com/mattheworiordan/capybara-screenshot/issues/211
-Capybara::Screenshot.register_driver(:headless_chrome) do |driver, path|
-  driver.browser.save_screenshot(path)
-end
